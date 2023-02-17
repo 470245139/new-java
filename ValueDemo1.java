9bffb5dc8580b0c1c4da6dabe5bfaa87557a2529
470245139 <60742257+470245139@users.noreply.github.com>
2023-02-17 19:52:14 +0800

    Add files via upload
    运行输出三句话

---
+++ ValueDemo1.java
@@ -0,0 +1,10 @@
+public class ValueDemo1{
+    public static void main(String[] args){
+        System.out.println("点赞");
+        System.out.println(666);
+        System.out.println(2.33333);
+    }
+
+
+
+}
