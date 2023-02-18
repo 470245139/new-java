0d4a280c9faf6f85dffcadb7df20d09803116cd3
470245139 <470245139@qq.com>
2023-02-18 20:55:33 +0800

    dev: print again

--- print_w.java
+++ print_w.java
@@ -3,10 +3,11 @@ import java.util.Scanner;
 public class print_w{
     public static void main(String[] args){
         int x=1;
-        int y=2;
+        int y=3;
         int z=5;
         Scanner sc = new Scanner(System.in);
         int scanned = sc.nextInt();
+
 
 
         System.out.println(scanned);
